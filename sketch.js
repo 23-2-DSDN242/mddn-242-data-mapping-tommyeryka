aab18245c97c724630c5f209f75652800c7180eb
tommyeryka <tommyeryk@myvuw.ac.nz>
2023-10-10 10:23:26 +1300

    edit with wave

--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_new.jpg";
-let maskFile   = "mask_new.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
 let outputFile = "output_1.png";
 
 function preload() { 
@@ -23,6 +23,47 @@ function setup () {
   maskImg.loadPixels();
 }
 
+let X_STOP = 1800;
+let Y_STOP = 1300;
+// let X_STOP = 1920;
+// let Y_STOP = 1080;
+let OFFSET = 20;
+
+//let renderCounter=0;
+function draw () {
+  angleMode(DEGREES);
+  let num_lines_to_draw = 80;
+  // get one scanline
+  for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<Y_STOP; j++) {
+    for(let i=0; i<X_STOP; i++) {
+      colorMode(RGB);
+      let mask = maskImg.get(i, j);
+      if (mask[1] < 128) {
+        pix = sourceImg.get(i, j);
+      }
+      else {
+        let wave = sin(j*8);
+        let slip = map(wave, -1, 1, -OFFSET, OFFSET);
+        pix = sourceImg.get(i+slip, j);
+
+
+  // for(let c=0; c<3; c++) {
+       //  pix[c] = brt;
+      // }
+      }
+
+      set(i, j, pix);
+    }
+  }
+  renderCounter = renderCounter + num_lines_to_draw;
+  updatePixels();
+
+  // print(renderCounter);
+  if(renderCounter > Y_STOP) {
+    console.log("Done!")
+    noLoop();
+  }
+/*
 function draw () {
   for(let i=0;i<40000;i++) {
     let x = floor(random(sourceImg.width));
@@ -47,9 +88,11 @@ function draw () {
     // saveArtworkImage(outputFile);
   }
 }
-
+*/
+  }
 function keyTyped() {
   if (key == '!') {
     saveBlocksImages();
   }
 }
+  
