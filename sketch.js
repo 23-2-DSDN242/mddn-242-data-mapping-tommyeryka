b2921f0270c414af618df62aa8ec44ea0120eac3
tommyeryka <tommyeryk@myvuw.ac.nz>
2023-10-10 20:27:54 +1300

    lots of changes to background

--- sketch.js
+++ sketch.js
@@ -3,29 +3,31 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
 let outputFile = "output_1.png";
 
 function preload() { 
   sourceImg = loadImage(sourceFile); 
   maskImg = loadImage(maskFile); 
+
+    textImg = loadImage("swirl.png");
 }
 
 function setup () {
-  let main_canvas = createCanvas(1920, 1080);
+  let main_canvas = createCanvas(1920, 1440);
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
   noStroke();
-  background(255, 0, 0);
+  background(255);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
 let X_STOP = 1800;
 let Y_STOP = 1300;
-// let X_STOP = 1920;
+// let X_STOP = 1920; 
 // let Y_STOP = 1080;
 let OFFSET = 15;
 
@@ -33,6 +35,41 @@ let OFFSET = 15;
 
 function draw () {
   
+ 
+  let num_lines_to_draw = 40;
+  // get one scanline
+  for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<1080; j++) {
+    for(let i=0; i<X_STOP; i++) {
+      colorMode(RGB);
+      let pix = sourceImg.get(i, j);
+      // create a color from the values (always RGB)
+      let col = color(pix);
+      let mask = maskImg.get(i, j);
+      let tex = textImg.get(i, j);
+
+      if(mask[0] < 128) {
+        set(i, j, pix);
+      }
+      else {
+        let new_col = [0, 0, 0, 255];
+        for(let k=0; k<3; k++) {
+          new_col[k] = map(40, 0, 100, pix[k], tex[k]);
+        }
+        // let new_col = color(h, s,  newBrt);
+        set(i, j, new_col);
+      }
+    }
+  
+  renderCounter = renderCounter + num_lines_to_draw;
+  updatePixels();
+
+  // print(renderCounter);
+  if(renderCounter > Y_STOP) {
+    console.log("Done!")
+    noLoop();
+
+    
+
   angleMode(DEGREES);
   let num_lines_to_draw = 40;
   // get one scanline
@@ -49,7 +86,8 @@ function draw () {
         let slip = map(wave, -1, 1, -OFFSET, OFFSET);
         pix = sourceImg.get(i+slip, j);
 
-
+     
+      
   // for(let c=0; c<3; c++) {
        //  pix[c] = brt;
       // }
@@ -59,7 +97,7 @@ function draw () {
     }
 
   
-  }
+        }
   renderCounter = renderCounter + num_lines_to_draw;
   updatePixels();
 
@@ -67,7 +105,10 @@ function draw () {
   if(renderCounter > Y_STOP) {
     console.log("Done!")
     noLoop();
+
+ 
   }
+}
 
 /*
 function draw () {
@@ -102,4 +143,5 @@ function keyTyped() {
   }
 }
   
-}
+      }
+    }
